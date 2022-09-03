ba1038a5baa5a46e69eafe7c2e85817cfc8332f8
Samuel Apolion Benevenuto <sasibenevenuto@gmail.com>
2022-09-03 07:03:27 -0300

    projeto teste

---
+++ WebApiMicroDataTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApiMicroDataTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
